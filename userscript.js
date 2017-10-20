55cd16897ea4544dc64e027ba42ca199a8bfefe8
BlackCapCoder <mailhph@gmail.com>
2017-10-20 16:29:39 +0200

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://scrimba.com/casts/*
+// @match        https://scrimba.com/c/*
 // @grant        none
 // ==/UserScript==
 
